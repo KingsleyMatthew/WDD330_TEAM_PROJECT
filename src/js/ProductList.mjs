349f04bc3ede380b3dfd4e595a9a89e7dfb2e216
Kelvin Gonzalez <kelvingonzalezm@gmail.com>
2026-07-17 21:48:42 -0400

    Complete W03 Expand Inventory

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,10 +1,12 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+  console.log(product);
+
   return `
     <li class="product-card">
-      <a href="product_pages/index.html?product=${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
+      <a href="/product_pages/index.html?product=${product.Id}">
+        <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
@@ -26,7 +28,7 @@ export default class ProductList {
     if (searchTerm) {
       list = await this.dataSource.searchProducts(searchTerm);
     } else {
-      list = await this.dataSource.getData();
+      list = await this.dataSource.getData(this.category);
     }
 
     this.renderList(list);
---
+++ src/js/product-listing.js
@@ -0,0 +1,36 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category");
+
+const title = document.querySelector(".product-list-title");
+
+if (title) {
+  title.textContent = `Top Products: ${category}`;
+}
+
+const dataSource = new ProductData();
+
+const listElement = document.querySelector(".product-list");
+
+const productList = new ProductList(category, dataSource, listElement);
+
+productList.init();
+
+const searchForm = document.querySelector(".search-form");
+
+if (searchForm) {
+  searchForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const searchTerm = document
+      .querySelector("#search")
+      .value
+      .trim();
+
+    await productList.init(searchTerm);
+  });
+}
