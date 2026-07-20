c6702715ff69c10a29f89e01448af05ba464f432
Kelvin Gonzalez <kelvingonzalezm@gmail.com>
2026-07-20 13:20:48 -0400

    Add discount indicator and category SVG styling

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,10 +3,27 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
   console.log(product);
 
+  const hasDiscount = product.FinalPrice < product.SuggestedRetailPrice;
+
+  const discountPercent = hasDiscount
+    ? Math.round(
+      ((product.SuggestedRetailPrice - product.FinalPrice) /
+        product.SuggestedRetailPrice) *
+        100
+    )
+  : 0;
+
   return `
     <li class="product-card">
       <a href="/product_pages/index.html?product=${product.Id}">
         <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+
+        ${
+          hasDiscount
+            ? `<span class="discount-badge">${discountPercent}% OFF</span>`
+            : ""
+        }
+        
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const dataSource = new ProductData("tents");
 
 const listElement = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, listElement);
+const productList = new ProductList("tents", dataSource, listElement);
 
 productList.init();
 
