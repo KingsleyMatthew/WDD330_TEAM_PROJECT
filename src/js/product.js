67724be3236ca1562f135caeba9d885e8130862b
Kelvin Gonzalez <kelvingonzalezm@gmail.com>
2026-07-21 19:37:08 -0400

    Refactor ProductData to ExternalServices

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,9 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 
 const productId = getParam("product");
 
